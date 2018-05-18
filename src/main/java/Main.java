bd46dcbd3bc9b981db277221f02c272cdcafd4b3
catp0wer <catp0wer@mail.ru>
2018-05-18 23:43:49 +0300

    minimal working sample with a part of xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ import org.apache.http.client.utils.URIBuilder;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
+import com.thoughtworks.xstream.XStream;
 
 public class Main {
 
@@ -17,6 +19,14 @@ public class Main {
         HttpRequest r1 = new HttpRequest();
         String result = r1.Request(bnmURL);
         System.out.println(result);
+
+
+
+        result = "<Valute ID=\"47\"><NumCode>784</NumCode><CharCode>AED</CharCode><Nominal>1</Nominal><Name>U.A.E. Dirham</Name><Value>4.5636</Value></Valute>";
+        XStream xstream = new XStream();
+        xstream.alias("Valute", Rate.class);
+        Rate myObj = (Rate) xstream.fromXML(result);
+        System.out.println(myObj);
 
     }
 }
--- src/main/java/Rate.java
+++ src/main/java/Rate.java
@@ -1,49 +1,74 @@
 public class Rate {
 
-    String valuteID;
-    String charCode;
-    int nominal;
-    String name;
-    int value;
+//    private String Valute;
+    private int NumCode;
+    private String CharCode;
+    private int Nominal;
+    private String Name;
+    private float Value;
+//    private String ValCurs;
 
-    public void setValuteID(String valuteID) {
-        this.valuteID = valuteID;
+    public void setNumCode(int numCode) {
+        NumCode = numCode;
     }
 
+    public int getNumCode() {
+        return NumCode;
+    }
+
+//    public String getValCurs() {
+//        return ValCurs;
+//    }
+
+
+
+//    public void setValCurs(String valCursDate) {
+//        ValCurs = valCursDate;
+//    }
+//
+//    public String getValCursDate() {
+//
+//        return ValCurs;
+//    }
+
+//    public void setValute(String valute) {
+//        this.Valute = Valute;
+//    }
+
     public void setCharCode(String charCode) {
-        this.charCode = charCode;
+        this.CharCode = charCode;
     }
 
     public void setNominal(int nominal) {
-        this.nominal = nominal;
+        this.Nominal = nominal;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.Name = name;
     }
 
     public void setValue(int value) {
-        this.value = value;
+        this.Value = value;
     }
 
-    public String getValuteID() {
-
-        return valuteID;
-    }
+//    public String getValute() {
+//
+//        return Valute;
+//    }
 
     public String getCharCode() {
-        return charCode;
+        return CharCode;
     }
 
     public int getNominal() {
-        return nominal;
+        return Nominal;
     }
 
     public String getName() {
-        return name;
+        return Name;
     }
 
-    public int getValue() {
-        return value;
+    public float getValue() {
+        return Value;
     }
 }
